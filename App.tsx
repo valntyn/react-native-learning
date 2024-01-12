a59e37cab3ffa070f27da335abd8dee6059fc6ab
valntyn <romanenko.valentyn1@gmail.com>
2024-01-12 15:48:37 +0200

    eslint and prettier configuration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const colors = {
+    white: '#fff',
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        alignItems: 'center',
+        backgroundColor: colors.white,
+        flex: 1,
+        justifyContent: 'center',
+    },
 });
+
+export default function App() {
+    console.log('hello');
+
+    return (
+        <View style={styles.container}>
+            <Text>Hello world</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+console.log(styles);
